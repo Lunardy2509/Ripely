3f33f8e8afd85c358fe4ec2084b6b77d2076915b
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2025-06-14 11:47:47 +0700

    Added OnBoarding Model

---
+++ Aripe/OnBoarding/OnBoardingModel.swift
@@ -0,0 +1,15 @@
+//
+//  OnBoardingModel.swift
+//  Aripe
+//
+//  Created by Reynard Hansel on 14/06/25.
+//
+
+import Foundation
+
+struct OnboardingPage: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let title: String
+    let description: String
+}
