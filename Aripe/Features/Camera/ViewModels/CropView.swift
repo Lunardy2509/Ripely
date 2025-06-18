db3b17084bdeec8ee2350749bfb5519a7a2d4686
Ferdinand Lunardy <ferdilunardy@gmail.com>
2025-06-18 22:03:11 +0700

    photopicker makin ganteng bs manual crop, offset camera diperganteng

---
+++ Aripe/Features/Camera/ViewModels/CropView.swift
@@ -0,0 +1,44 @@
+//
+//  CropView.swift
+//  Aripe
+//
+//  Created by Ferdinand Lunardy on 18/06/25.
+//
+
+import SwiftUI
+import TOCropViewController
+
+struct CropView: UIViewControllerRepresentable {
+    let image: UIImage
+    var onCropped: (UIImage) -> Void
+
+    func makeUIViewController(context: Context) -> TOCropViewController {
+        let cropVC = TOCropViewController(image: image)
+        cropVC.delegate = context.coordinator
+        return cropVC
+    }
+
+    func updateUIViewController(_ uiViewController: TOCropViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, TOCropViewControllerDelegate {
+        let parent: CropView
+
+        init(_ parent: CropView) {
+            self.parent = parent
+        }
+
+        func cropViewController(_ cropViewController: TOCropViewController, didCropTo image: UIImage, with cropRect: CGRect, angle: Int)
+        {
+            parent.onCropped(image)
+            cropViewController.dismiss(animated: true)
+        }
+
+        func cropViewControllerDidCancel(_ cropViewController: TOCropViewController) {
+            cropViewController.dismiss(animated: true)
+        }
+    }
+}
